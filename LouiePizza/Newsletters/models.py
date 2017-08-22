e6223af0f98eb1f1c80b5b444dbbc53117f819a4
Alex Hagerman <alexander.hagerman@icloud.com>
2017-08-22 16:08:40 -0400

    Initial steps to integrating the FEWD site into a Django project completed. Assets are rendering initial templates and app setup.

---
+++ LouiePizza/Newsletters/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Subscriber(models.Model):
+    subscribed_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=255)
+    email = models.TextField()
+
+    def __str__(self):
+        return self.name
